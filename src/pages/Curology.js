0e408b5ee738e0df195213d997e81bc36f0034da
L-Villa <63620359+L-Villa@users.noreply.github.com>
2020-09-24 11:24:09 -0700

    Added case landing animations

--- src/pages/Curology.js
+++ src/pages/Curology.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
 import curology from "../assets/curology-min.png";
 import { ReactComponent as CasesNext } from "../assets/arrow-right.svg";
 
+const tl = gsap.timeline();
+
 export default function Curology() {
+  useEffect(() => {
+    tl.from(".case-study-text-container", {
+      duration: 2,
+      delay: 0.2,
+      opacity: 0,
+    })
+      .from(".line span", {
+        duration: 2,
+        delay: -2,
+        y: 125,
+        skewY: 7,
+        stagger: 0.3,
+        ease: "power4.out",
+      })
+      .to(".scroll-indicator", {
+        duration: 2,
+        delay: -2,
+        opacity: 1,
+      });
+  });
+
   return (
     <div>
       <section className="case-study-landing">
@@ -10,11 +34,18 @@ export default function Curology() {
         <div className="container">
           <div className="case-study-text-container">
             <p>Curology</p>
-            <h2>A custom formula for your skin's unique needs</h2>
+            <h2>
+              <div className="line">
+                <span>A custom formula for </span>
+              </div>
+              <div className="line">
+                <span>your skin's unique needs</span>
+              </div>
+            </h2>
           </div>
           <div className="scroll-indicator">
             <p>Scroll Down</p>
-            <div className="cases-arrow next">
+            <div className="scroll-arrow">
               <CasesNext />
             </div>
           </div>
