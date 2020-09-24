798950a51473985b3789b6cfc5268ea5568b94a2
L-Villa <63620359+L-Villa@users.noreply.github.com>
2020-09-23 21:20:12 -0700

    Updated gsap function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,33 +7,37 @@ import IntroOverlay from "../components/IntroOverlay";
 const tl = gsap.timeline();
 
 const homeAnimation = (completeAnimation) => {
-  tl.from(".line span", 1.8, {
-    y: 100,
+  tl.from(".line span", {
+    duration: 1.8,
+    y: 125,
     ease: "power4.out",
     delay: 1,
     skewY: 7,
     stagger: 0.3,
   })
-    .to(".overlay-top", 1.6, {
+    .to(".overlay-top", {
+      duration: 1.6,
       height: 0,
       ease: "expo.inOut",
       stagger: 0.4,
     })
-    .to(".overlay-bottom", 1.6, {
+    .to(".overlay-bottom", {
+      duration: 1.6,
       width: 0,
       ease: "expo.inOut",
       delay: -0.8,
       stagger: 0.4,
     })
-    .to(".intro-overlay", 0, { css: { display: "none" } })
-    .from(".case-image img", 1.6, {
+    .to(".intro-overlay", { duration: 0, css: { display: "none" } })
+    .from(".case-image img", {
+      duration: 1.6,
       scale: 1.4,
       ease: "expo.inOut",
       delay: -2,
       stagger: 0.4,
-      onComplete: completeAnimation
+      onComplete: completeAnimation,
     });
-}
+};
 
 export default function Home(props) {
   const [animationComplete, setAnimationComplete] = useState(false);
